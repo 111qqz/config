c6751da373263dc3a7919ba8ebec759becf10773
111qqz <hust.111qqz@gmail.com>
2018-02-14 16:10:35 +0800

    update manjaro_install.sh

--- install_manjaro.sh
+++ install_manjaro.sh
@@ -1,14 +1,60 @@
-set guifont=Neep\ 18
+pacman-mirrors -c China
+echo " [archlinuxcn] " >> /etc/pacman.conf
+echo " SigLevel = Optional TrustedOnly "  >> /etc/pacman.conf
+echo " Server = https://mirrors.ustc.edu.cn/archlinuxcn/\$arch" >> /etc/pacman.conf
+
+
+echo "[arch4edu]" >> /etc/pacman.conf
+echo "SigLevel = Never" >> /etc/pacman.conf
+echo "Server = http://mirrors.tuna.tsinghua.edu.cn/arch4edu/\$arch " >> /etc/pacman.conf
+pacman -Syyu
+pacman -S archlinuxcn-keyring
+
+ pacman -S yakuake fish gvim 
+ pacman -S google-chrome chromium
+ pacman -S  wget aria2  remarkable netease-cloud-music
+ pacman -S fcitx fcitx-configtool fcitx-sogoupinyin fcitx-im kcm-fcitx
+ pacman -S shadowsocks-qt5
+ pacman -S wqy-microhei wqy-microhei-lite wqy-bitmapfont wqy-zenhei ttf-arphic-ukai ttf-arphic-uming adobe-source-han-sans-cn-fonts
+
+cat >> ~/.xprofile  <<EOF
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS="@im=fcitx"
+EOF
+
+ pacman -S proxychains-ng
+cat >> /etc/proxychains.conf <<EOF
+quiet_mode
+dynamic_chain
+chain_len = 1 #round_robin_chain和random_chain使用
+proxy_dns
+remote_dns_subnet 224
+tcp_read_time_out 15000
+tcp_connect_time_out 8000
+localnet 127.0.0.0/255.0.0.0
+localnet 10.0.0.0/255.0.0.0
+localnet 172.16.0.0/255.240.0.0
+localnet 192.168.0.0/255.255.0.0
  
+[ProxyList]
+socks5  127.0.0.1 1080
+http    127.0.0.1 1087
+EOF
+ 
+pacman -S vundle-git
+cat >> ~/.vimrc  <<EOF
+set guifont=Neep\ 18
+
 map <F9> :call SaveInputData()<CR>
 func! SaveInputData()
     exec "tabnew"
     exec 'normal "+gP'
     exec "w! code/in.txt"
 endfunc
- 
- 
- 
+
+
+
 "colorscheme torte
 " colorscheme murphy
 colorscheme elflord
@@ -20,44 +66,44 @@ colorscheme elflord
 "colorscheme darkblue
 "colorscheme evening
 "colorscheme shine
- 
+
 "colorscheme molokai
 "colorscheme solarized
 "colorscheme sift
 "colorscheme advantage
 "colorscheme 256_jungle
- 
- 
+
+
 "set fencs=utf-8,ucs-bom,shift-jis,gb18030,gbk,gb2312,cp936
 "set termencoding=utf-8
 "set encoding=utf-8
 "set fileencodings=ucs-bom,utf-8,cp936
 "set fileencoding=utf-8
- 
+
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-" 显示相关  
+" 显示相关
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-"set shortmess=atI   " 启动的时候不显示那个援助乌干达儿童的提示  
-"winpos 5 5          " 设定窗口位置  
-"set lines=40 columns=155    " 设定窗口大小  
-set go=             " 不要图形按钮  
-"color asmanian2     " 设置背景主题  
-"set guifont=Courier_New:h10:cANSI   " 设置字体  
-syntax on           " 语法高亮  
-autocmd InsertLeave * se nocul  " 用浅色高亮当前行  
-autocmd InsertEnter * se cul    " 用浅色高亮当前行  
-"set ruler           " 显示标尺  
-set showcmd         " 输入的命令显示出来，看的清楚些  
-"set cmdheight=1     " 命令行（在状态行下）的高度，设置为1  
-"set whichwrap+=<,>,h,l   " 允许backspace和光标键跨越行边界(不建议)  
-"set scrolloff=3     " 光标移动到buffer的顶部和底部时保持3行距离  
-set novisualbell    " 不要闪烁(不明白)  
-set statusline=%F%m%r%h%w\ [FORMAT=%{&ff}]\ [TYPE=%Y]\ [POS=%l,%v][%p%%]\ %{strftime(\"%d/%m/%y\ -\ %H:%M\")}   "状态行显示的内容  
-set laststatus=1    " 启动显示状态行(1),总是显示状态行(2)  
-"set foldenable      " 允许折叠  
-set foldmethod=manual   " 手动折叠  
-"set background=dark "背景使用黑色 
-set nocompatible  "去掉讨厌的有关vi一致性模式，避免以前版本的一些bug和局限  
+"set shortmess=atI   " 启动的时候不显示那个援助乌干达儿童的提示
+"winpos 5 5          " 设定窗口位置
+"set lines=40 columns=155    " 设定窗口大小
+set go=             " 不要图形按钮
+"color asmanian2     " 设置背景主题
+"set guifont=Courier_New:h10:cANSI   " 设置字体
+syntax on           " 语法高亮
+autocmd InsertLeave * se nocul  " 用浅色高亮当前行
+autocmd InsertEnter * se cul    " 用浅色高亮当前行
+"set ruler           " 显示标尺
+set showcmd         " 输入的命令显示出来，看的清楚些
+"set cmdheight=1     " 命令行（在状态行下）的高度，设置为1
+"set whichwrap+=<,>,h,l   " 允许backspace和光标键跨越行边界(不建议)
+"set scrolloff=3     " 光标移动到buffer的顶部和底部时保持3行距离
+set novisualbell    " 不要闪烁(不明白)
+set statusline=%F%m%r%h%w\ [FORMAT=%{&ff}]\ [TYPE=%Y]\ [POS=%l,%v][%p%%]\ %{strftime(\"%d/%m/%y\ -\ %H:%M\")}   "状态行显示的内容
+set laststatus=1    " 启动显示状态行(1),总是显示状态行(2)
+"set foldenable      " 允许折叠
+set foldmethod=manual   " 手动折叠
+"set background=dark "背景使用黑色
+set nocompatible  "去掉讨厌的有关vi一致性模式，避免以前版本的一些bug和局限
 " 显示中文帮助
 if version >= 603
     set helplang=cn
@@ -65,39 +111,39 @@ if version >= 603
 endif
 " 设置配色方案
 "colorscheme murphy
-"字体 
-"if (has("gui_running")) 
-"   set guifont=Bitstream\ Vera\ Sans\ Mono\ 10 
-"endif 
+"字体
+"if (has("gui_running"))
+"   set guifont=Bitstream\ Vera\ Sans\ Mono\ 10
+"endif
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 """""新文件标题
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-"新建.c,.h,.sh,.java文件，自动插入文件头 
-autocmd BufNewFile *.cpp,*.[ch],*.sh,*.java exec ":call SetTitle()" 
+"新建.c,.h,.sh,.java文件，自动插入文件头
+autocmd BufNewFile *.cpp,*.[ch],*.sh,*.java exec ":call SetTitle()"
 ""定义函数SetTitle，自动插入文件头
 "map <F4> :call SetTitle()<CR>
-func SetTitle() 
-    "如果文件类型为.sh文件 
-    if &filetype == 'sh' 
-        call setline(1,"\#########################################################################") 
-        call append(line("."), "\# File Name: ".expand("%")) 
-        call append(line(".")+1, "\# Author: 111qqz") 
-        call append(line(".")+2, "\# mail: renkuanze@sensetime.com") 
-        call append(line(".")+3, "\# Created Time: ".strftime("%c")) 
-        call append(line(".")+4, "\#########################################################################") 
-        call append(line(".")+5, "\#!/bin/bash") 
-        call append(line(".")+6, "") 
-    else 
+func SetTitle()
+    "如果文件类型为.sh文件
+    if &filetype == 'sh'
+        call setline(1,"\#########################################################################")
+        call append(line("."), "\# File Name: ".expand("%"))
+        call append(line(".")+1, "\# Author: 111qqz")
+        call append(line(".")+2, "\# mail: renkuanze@sensetime.com")
+        call append(line(".")+3, "\# Created Time: ".strftime("%c"))
+        call append(line(".")+4, "\#########################################################################")
+        call append(line(".")+5, "\#!/bin/bash")
+        call append(line(".")+6, "")
+    else
         let l = 0
         let l = l + 1 | call setline(l,'/* ***********************************************')
         let l = l + 1 | call setline(l,'Author :111qqz')
-	let l = l + 1 | call setline(l,'mail: renkuanze@sensetime.com')
+				let l = l + 1 | call setline(l,'mail: renkuanze@sensetime.com'
         let l = l + 1 | call setline(l,'Created Time :'.strftime('%c'))
         let l = l + 1 | call setline(l,'File Name :'.expand('%'))
         let l = l + 1 | call setline(l,'************************************************ */')
         let l = l + 1 | call setline(l,'')
     endif
-    if &filetype == 'cpp' 
+    if &filetype == 'cpp'
         let l = l + 1 | call setline(l,'#include <bits/stdc++.h>')
         let l = l + 1 | call setline(l,'')
         let l = l + 1 | call setline(l,'using namespace std;')
@@ -105,8 +151,8 @@ func SetTitle()
         let l = l + 1 | call setline(l,'{')
         let l = l + 1 | call setline(l,'    return 0;')
         let l = l + 1 | call setline(l,'}')
- 
- 
+
+
     endif
     if &filetype == 'c'
         call append(line(".")+6, "#include<stdio.h>")
@@ -122,34 +168,34 @@ endfunc
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 "键盘命令
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""v""""""""""""""""""""""""""""""""""""""""
- 
+
 nmap <leader>w :w!<cr>
 nmap <leader>f :find<cr>
- 
+
 nmap <F8> :TagbarToggle<CR>
- 
- 
+
+
 " 映射全选+复制 ctrl+a
 map <C-A> ggvG"+Y
 map! <C-A> <Esc>ggVGY
 map <F12> gg=G
 " 选中状态下 Ctrl+c 复制
 vmap <C-c> "+y
-"去空行  
-nnoremap <F2> :g/^\s*$/d<CR> 
-"比较文件  
-nnoremap <C-F2> :vert diffsplit 
-"新建标签  
-map <M-F2> :tabnew<CR>  
-"列出当前目录文件  
-map <F3> :tabnew .<CR>  
-"打开树状文件目录  
-map <C-F3> \be  
+"去空行
+nnoremap <F2> :g/^\s*$/d<CR>
+"比较文件
+nnoremap <C-F2> :vert diffsplit
+"新建标签
+map <M-F2> :tabnew<CR>
+"列出当前目录文件
+map <F3> :tabnew .<CR>
+"打开树状文件目录
+map <C-F3> \be
 "C，C++ 按F5编译运行
 map <F5> :call CompileRunGcc()<CR>
- 
+
 au BufRead *.py map <buffer> <F6> :w<CR>:!/usr/bin/env python % <CR>
- 
+
 let g:tagbar_usearrows = 1
 nnoremap <leader>l :TagbarToggle<CR>
 func! CompileRunGcc()
@@ -160,8 +206,8 @@ func! CompileRunGcc()
     elseif &filetype == 'cpp'
         exec "!g++ % -std=gnu++11  -Wall   -o %<"
         exec "! ./%<"
-    elseif &filetype == 'java' 
-        exec "!javac %" 
+    elseif &filetype == 'java'
+        exec "!javac %"
         exec "!java %<"
     elseif &filetype == 'sh'
         :!./%
@@ -178,9 +224,9 @@ func! Rungdb()
     exec "!g++ % -std=c++11 -g  -o %<"
     exec "!gdb ./%<"
 endfunc
- 
- 
- 
+
+
+
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 ""实用设置
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -188,13 +234,13 @@ endfunc
 set autoread
 " quickfix模式
 autocmd FileType c,cpp map <buffer> <leader><space> :w<cr>:make<cr>
-"代码补全 
-set completeopt=preview,menu 
-"允许插件  
+"代码补全
+set completeopt=preview,menu
+"允许插件
 filetype plugin on
-"共享剪贴板  
-set clipboard+=unnamed 
-"从不备份  
+"共享剪贴板
+set clipboard+=unnamed
+"从不备份
 set nobackup
 "make 运行
 :set makeprg=g++\ -Wall\ \ %
@@ -208,8 +254,8 @@ set guioptions-=m           " 隐藏菜单栏
 "set statusline=\ %<%F[%1*%M%*%n%R%H]%=\ %y\ %0(%{&fileformat}\ %{&encoding}\ %c:%l/%L%)\
 " 设置在状态行显示的信息
 set foldcolumn=0
-set foldmethod=indent 
-set foldlevel=3 
+set foldmethod=indent
+set foldlevel=3
 set foldenable              " 开始折叠
 " 不要使用vi的键盘模式，而是vim自己的
 set nocompatible
@@ -313,23 +359,23 @@ function! ClosePair(char)
         return a:char
     endif
 endfunction
-filetype plugin indent on 
+filetype plugin indent on
 "打开文件类型检测, 加了这句才可以用智能补全
 set completeopt=longest,menu
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
- 
- 
- 
- 
- 
- 
- 
+
+
+
+
+
+
+
 """""""""""""""""vundle设置"""""""""""""""""
 set nocompatible              " be iMproved, required
 "filetype off                  " required
-set shell=/bin/bash 
- 
- 
+set shell=/bin/bash
+
+
 " Interface
 set rtp+=~/.vim/bundle/Vundle.vim
 call vundle#begin()
@@ -337,11 +383,11 @@ Plugin 'VundleVim/Vundle.vim'
 " vim-scripts repos
 "
 "
- 
+
   Plugin 'MarcWeber/vim-addon-mw-utils'
   Plugin 'tomtom/tlib_vim'
   Plugin 'garbas/vim-snipmate'
- 
+
   " Optional:
  Plugin 'honza/vim-snippets'
  Plugin 'davidhalter/jedi-vim'
@@ -351,38 +397,38 @@ Plugin 'VundleVim/Vundle.vim'
  Plugin 'perl-support.vim'
  Plugin 'majutsushi/tagbar'
  Plugin 'ZoomWin'
- Plugin 'morhetz/gruvbox' 
+ Plugin 'morhetz/gruvbox'
  Plugin 'scrooloose/syntastic'
 Plugin 'Valloric/YouCompleteMe'
 call vundle#end()            " required
 filetype plugin indent on    " required
- 
- 
- 
- 
+
+
+
+
 """""""""""""""""youcompleteme设置"""""""""""""""""
- 
- 
+
+
 "let g:ycm_autoclose_preview_window_after_completion=1
 "nnoremap <leader>g :YcmCompleter GoToDefinitionElseDeclaration<CR>
- 
+
 " YouCompleteMe 功能
 " 补全功能在注释中同样有效
 "let g:ycm_global_ycm_extra_conf = '~/.vim/bundle/YouCompleteMe/cpp/ycm/.ycm_extra_conf.py'
 "let g:ycm_complete_in_comments=1
  "允许 vim 加载 .ycm_extra_conf.py 文件，不再提示
 "let g:ycm_confirm_extra_conf=0
- 
+
 "let g:ycm_error_symbol = '<<'
 "let g:ycm_warning_symbol = '<'
 "nnoremap <leader>gl :YcmCompleter GoToDeclaration<CR>
 "nnoremap <leader>gf :YcmCompleter GoToDefinition<CR>
 "nnoremap <leader>gg :YcmCompleter GoToDefinitionElseDeclaration<CR>
 "nmap <F4> :YcmDiags<CR>
- 
- 
- 
- 
+
+
+
+
 """""""""""""""""youcompleteme设置 by wyz"""""""""""""""""
 let g:cpp_class_scope_highlight=1
 let g:cpp_experimental_template_highlight=1
@@ -398,13 +444,13 @@ let g:ycm_collect_identifiers_from_tags_files = 1
 let g:ycm_confirm_extra_conf = 0
 let g:ycm_autoclose_preview_window_after_completion = 1
 let g:ycm_autoclose_preview_window_after_insertion = 1
- 
- 
- 
+
+
+
 " for python
 "
 "
- 
+
 "默认配置文件路径"
 let g:ycm_global_ycm_extra_conf = '~/.ycm_extra_conf.py'
 "打开vim时不再询问是否加载ycm_extra_conf.py配置"
@@ -425,34 +471,31 @@ let g:ycm_autoclose_preview_window_after_completion=1
 let g:ycm_cache_omnifunc=0
 "字符串中也开启补全"
 let g:ycm_complete_in_strings = 1
- 
- 
- 
+
+
+
 """"""""""""""'for airline""""""""""""""""
- 
-let g:airline_powerline_fonts = 1   
+
+let g:airline_powerline_fonts = 1
 let g:airline#extensions#tabline#enabled = 1
  let g:airline#extensions#tabline#buffer_nr_show = 1
 let g:airline#extensions#tabline#left_sep = ' '
 let g:airline#extensions#tabline#left_alt_sep = '|'
- 
+
  nnoremap <F2> :bnext<CR>
  nnoremap <F4> :bprevious<CR>
- 
+
  if !exists('g:airline_symbols')
     let g:airline_symbols = {}
     endif
- 
+
  let g:airline_left_sep = '⮀'
   let g:airline_left_alt_sep = '⮁'
   let g:airline_right_sep = '⮂'
   let g:airline_right_alt_sep = '⮃'
   let g:airline_symbols.branch = '⭠'
   let g:airline_symbols.readonly = '⭤'
- 
- 
- 
- 
- 
- 
- 
+
+EOF
+
+echo " please install vim plug-in"
