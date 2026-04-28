51deb8cf7df0c18d3d8332659059b9afc1b60ad5
renkz@wanyantech.com <renkz@wanyantech.com>
2026-04-28 16:06:27 +0800

    feat(nvim): add lualine replacing vim-airline

---
+++ config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    event = 'VeryLazy',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      options = {
+        theme = 'tokyonight',
+        globalstatus = true,
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { { 'filename', path = 1 } },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+      tabline = {
+        lualine_a = { 'buffers' },
+        lualine_z = { 'tabs' },
+      },
+    },
+  },
+}
